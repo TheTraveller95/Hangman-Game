3a35180802fbcca699a0a74a6e5854c9f6530288
TheTraveller95 <crivellaroandres@gmail.com>
2019-11-07 15:02:29 +0000

    playNow function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var wordsIndex = ["mouse","computer","desk","smartphone","apple pencil", "monitor"];
 
+document.onload=firstLoading();
+//document.getElementById("play-now-link").onclick = playNow();
+
 function firstLoading(){
     document.getElementById("home-page");
     document.getElementById("footer-section");
@@ -7,16 +10,19 @@ function firstLoading(){
     draw();
 }
 
-function playNow(){
-    document.getElementById("#game-page");
-}
+$("#play-now-link").click (function playNow(){
+    document.getElementById("home-page").style.display="none";
+    document.getElementById("footer-section");
+    document.getElementById("game-page").style.display="block";
+    draw1();
+})
 
 
 function draw(){
     var ctx = document.getElementById("hangman-draw").getContext('2d');
         ctx.fillStyle = "transparent";
         ctx.lineWidth=1.5;
-        ctx.fillRect(0, 0, 100, 100);
+        ctx.fillRect(0, 0, 300, 200);
         ctx.beginPath();//first bottom bar
             ctx.moveTo(5,130);
             ctx.lineTo(185,130);
@@ -35,6 +41,33 @@ function draw(){
             ctx.stroke();
         ctx.beginPath(); // diagonal line
             ctx.moveTo(20,60);
+            ctx.lineTo(70,20);
+            ctx.stroke();
+}
+
+function draw1(){
+    var ctx = document.getElementById("hangman-game").getContext('2d');
+        ctx.fillStyle = "transparent";
+        ctx.lineWidth=1.5;
+        ctx.fillRect(0, 0, 300, 200);
+        ctx.beginPath();//first bottom bar
+            ctx.moveTo(20,130);
+            ctx.lineTo(170,130);
+            ctx.stroke();
+        ctx.beginPath(); //vertical bar
+            ctx.moveTo(35,20);
+            ctx.lineTo(35,130);
+            ctx.stroke();
+        ctx.beginPath(); //top bar
+            ctx.moveTo(35,20);
+            ctx.lineTo(220,20);
+            ctx.stroke();
+        ctx.beginPath(); //top vertical bar
+            ctx.moveTo(220,20);
+            ctx.lineTo(220,50);
+            ctx.stroke();
+        ctx.beginPath(); // diagonal line
+            ctx.moveTo(35,60);
             ctx.lineTo(70,20);
             ctx.stroke();
 }
